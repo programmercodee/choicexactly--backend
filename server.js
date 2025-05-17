b778d03b080f1c877461ff3ab4b67d504b28b92a
Brijesh <programmercode102@gmail.com>
2025-05-18 00:01:58 +0530

    first commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "https://choicexactly-frontend.vercel.app/",
+    origin: "https://choicexactly-frontend.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
