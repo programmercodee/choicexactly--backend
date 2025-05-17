f40f6d4e14af160dc95a010298136aef574e43ea
Brijesh <programmercode102@gmail.com>
2025-05-17 23:42:05 +0530

    first commit

---
+++ helpers/paypal.js
@@ -0,0 +1,9 @@
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+  mode: "live",
+  client_id: "AbqK9GVKh5-uw_0cDHHzrGTZaejxMWv6ApIJ9GseZrycgv3ZkE_sx7fKXKEwizbWa5daAT7GdpO7tGVY",
+  client_secret: "ENeu9LzHu-IFvLjV6g89-VMZa4XZsNfNh2FaWhJ_GKTYbao16abL4mebfo-__D69WnfWdCFfMzgvFyVe",
+});
+
+module.exports = paypal;
