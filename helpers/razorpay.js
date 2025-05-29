cc1ef670e9709f13421a969dfc1b8f25fccf7615
Brijesh <programmercode102@gmail.com>
2025-05-29 17:44:49 +0530

    razorpay inegration

--- helpers/razorpay.js
+++ helpers/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
-if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
-  throw new Error("Missing Razorpay credentials in environment variables.");
-}
+// if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+//   throw new Error("Missing Razorpay credentials in environment variables.");
+// }
 
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT;
 app.use(
   cors({
     origin: "https://choicexactly-frontend.vercel.app" ,
-    origin: "http://localhost:5173" ,
+    // origin: "http://localhost:5173" ,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
