e45fa6132431ac7b12ca08580feef39b9f0e7f7c
Brijesh <programmercode102@gmail.com>
2025-05-29 17:41:40 +0530

    razorpay inegration

--- helpers/razorpay.js
+++ helpers/razorpay.js
@@ -1,5 +1,9 @@
 const Razorpay = require("razorpay");
 
+if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+  throw new Error("Missing Razorpay credentials in environment variables.");
+}
+
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
