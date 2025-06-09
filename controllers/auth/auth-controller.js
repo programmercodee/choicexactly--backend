ee075e73b81b564a81849ec76cd81ce39f976ee0
Brijesh <programmercode102@gmail.com>
2025-06-09 11:26:13 +0530

    fixed vercel reload

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -69,7 +69,7 @@ const loginUser = async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: true, secure: false, sameSite: "lax",
+      httpOnly: true, secure: true, sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     }).json({
       success: true,
